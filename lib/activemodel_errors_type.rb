73db1e2fc3688771d8ccf131431b868805aa807d
Cyril Le Roy <cyril.leroy@ringcentral.com>
2021-05-19 15:54:43 +0200

    remove alias method chain

--- lib/activemodel_errors_type.rb
+++ lib/activemodel_errors_type.rb
@@ -2,5 +2,5 @@ require "activemodel_errors_type/version"
 require "activemodel_errors_type/errors_type"
 
 if defined? ActiveModel::Errors
-  ActiveModel::Errors.send(:include, ActiveModelErrorsType::ErrorsType)
+  ActiveModel::Errors.prepend(ActiveModelErrorsType::ErrorsType)
 end
--- lib/activemodel_errors_type/errors_type.rb
+++ lib/activemodel_errors_type/errors_type.rb
@@ -1,26 +1,20 @@
 module ActiveModelErrorsType
   module ErrorsType
 
-    def self.included(base)
-      base.alias_method_chain :initialize, :errors
-      base.alias_method_chain :generate_message, :errors
-      base.alias_method_chain :clear, :errors
-    end
-
-    def initialize_with_errors(base)
+    def initialize(base)
       @errors_type = {}
-      initialize_without_errors(base)
+      super(base)
     end
 
-    def generate_message_with_errors(attribute, type = :invalid, options = {})
+    def generate_message(attribute, type = :invalid, options = {})
       @errors_type[attribute.to_sym] ||= []
       @errors_type[attribute.to_sym] << type
-      generate_message_without_errors(attribute, type, options)
+      super(attribute, type, options)
     end
 
-    def clear_with_errors
+    def clear
       @errors_type.clear
-      clear_without_errors
+      super
     end
 
     def with_types
